c27cb766f69480f913999bdc25265e34a8f7c97d
Do Quyen <doquyen2404@gmail.com>
2022-01-26 06:48:46 +0700

    Create Product

---
+++ src/main/java/Objects/Product.java
@@ -0,0 +1,32 @@
+package objects;
+
+public class Product {
+    String name;
+    String desc;
+    String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
